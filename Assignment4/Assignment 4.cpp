3be55ba42b9d828cec94b106d4506b69162330f2
王玮琦 <wangweiqi20020221@126.com>
2024-04-05 18:58:01 -0700

    Start Assignment 4

---
+++ Assignment4/Assignment 4.cpp
@@ -0,0 +1,26 @@
+﻿// Assignment 4.cpp: 定义应用程序的入口点。
+//
+
+#include "Assignment 4.h"
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int *arrey = new int[100000];
+	srand((unsigned)time(NULL));
+	for (int i = 0; i < 100000; i++) {
+		arrey[i] = rand();
+	}
+
+	int *arrey1 = new int[100000];
+	for (int i = 0; i < 100000; i++) {
+		arrey1[i] = arrey[i];
+	}
+	sort(arrey1, arrey1 + 100000);
+
+	delete[] arrey1;
+	delete[] arrey;
+	return 0;
+}
---
+++ Assignment4/Assignment 4.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+#include <iostream>
+
+template <class type>
+void quick_sort_original(type& arrey, const int left, const int right) {
+	if (left >= right) {
+		return;
+	}
+	int temp_left = left;
+	int temp_right = right;
+	// Set standard
+	type standard = arrey[left];
+	temp_left++;
+	// Set left and right
+	while (temp_left < temp_right && arrey[temp_left] < standard) {
+		temp_left++;
+	}
+	while (temp_left < temp_right && arrey[temp_right] > standard) {
+		temp_right--;
+	}
+	// Swap
+	type temp = arrey[temp_left];
+	arrey[temp_left] = arrey[temp_right];
+	arrey[temp_right] = temp;
+	for (int index = left; index < temp_left; index++) {
+		arrey[index] = arrey[index + 1];
+	}
+	arrey[temp_left] = standard;
+	quick_sort_original(arrey, left, temp_left);
+	quick_sort_original(arrey, temp_left + 1, right);
+};
