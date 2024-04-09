af79d603fcec4fad1c51499febbbb3114a9c487c
王王韦王奇 <wangweiqi20020221@126.com>
2024-04-09 07:02:00 -0700

    Assignment 4 update

--- Assignment4/Assignment 4.cpp
+++ Assignment4/Assignment 4.cpp
@@ -18,9 +18,34 @@ int main() {
 	for (int i = 0; i < 100000; i++) {
 		arrey1[i] = arrey[i];
 	}
-	sort(arrey1, arrey1 + 100000);
 
+    clock_t startTime, endTime;
+    startTime = clock();
+	sort(arrey1, arrey1 + 100000);
+    endTime = clock();
+    cout << "Time use for std::sort(): " << endTime - startTime << endl;
 	delete[] arrey1;
+
+    int *arrey2 = new int[100000];
+    for (int i = 0; i < 100000; i++) {
+        arrey2[i] = arrey[i];
+    }
+    startTime = clock();
+    quick_sort_original(arrey2, 0, 99999);
+    endTime = clock();
+    cout << "Time use for original quick sort: " << endTime - startTime << endl;
+    delete[] arrey2;
+
+    int *arrey3 = new int[100000];
+    for (int i = 0; i < 100000; i++) {
+        arrey3[i] = arrey[i];
+    }
+    startTime = clock();
+    quick_sort_standard_optimization(arrey3, 0, 99999);
+    endTime = clock();
+    cout << "Time use for std::sort(): " << endTime - startTime << endl;
+    delete[] arrey3;
+
 	delete[] arrey;
 	return 0;
 }
--- Assignment4/Assignment 4.h
+++ Assignment4/Assignment 4.h
@@ -2,31 +2,69 @@
 
 #include <iostream>
 
-template <class type>
-void quick_sort_original(type& arrey, const int left, const int right) {
+void quick_sort_original(int arrey[], const int left, const int right) {
 	if (left >= right) {
 		return;
 	}
 	int temp_left = left;
 	int temp_right = right;
 	// Set standard
-	type standard = arrey[left];
-	temp_left++;
+	int standard = arrey[left];
 	// Set left and right
-	while (temp_left < temp_right && arrey[temp_left] < standard) {
-		temp_left++;
-	}
-	while (temp_left < temp_right && arrey[temp_right] > standard) {
-		temp_right--;
-	}
-	// Swap
-	type temp = arrey[temp_left];
-	arrey[temp_left] = arrey[temp_right];
-	arrey[temp_right] = temp;
-	for (int index = left; index < temp_left; index++) {
-		arrey[index] = arrey[index + 1];
-	}
-	arrey[temp_left] = standard;
-	quick_sort_original(arrey, left, temp_left);
+    while(temp_left < temp_right) {
+        while (temp_left < temp_right && arrey[temp_right] >= standard) {
+            temp_right--;
+        }
+        while (temp_left < temp_right && arrey[temp_left] <= standard) {
+            temp_left++;
+        }
+        // Swap
+        int temp = arrey[temp_left];
+        arrey[temp_left] = arrey[temp_right];
+        arrey[temp_right] = temp;
+    }
+    int temp = arrey[left];
+    arrey[left] = arrey[temp_left];
+    arrey[temp_left] = temp;
+	quick_sort_original(arrey, left, temp_left - 1);
 	quick_sort_original(arrey, temp_left + 1, right);
+};
+
+void quick_sort_standard_optimization(int arrey[], const int left, const int right) {
+    if (left >= right) {
+        return;
+    }
+    int temp_left = left;
+    int temp_right = right;
+    // Set standard
+    if ((arrey[left] <= arrey[(left+right)/2] && arrey[(left+right)/2] <= arrey[right]) || (arrey[right] <= arrey[(left+right)/2] && arrey[(left+right)/2] <= arrey[left])) {
+        int temp = arrey[left];
+        arrey[left] = arrey[(left+right)/2];
+        arrey[(left+right)/2] = temp;
+    } else if ((arrey[(left+right)/2] <= arrey[left] && arrey[left] <= arrey[right]) || (arrey[right] <= arrey[left] && arrey[left] <= arrey[(left+right)/2])) {
+
+    } else {
+        int temp = arrey[left];
+        arrey[left] = arrey[right];
+        arrey[right] = temp;
+    }
+    int standard = arrey[left];
+    // Set left and right
+    while(temp_left < temp_right) {
+        while (temp_left < temp_right && arrey[temp_right] >= standard) {
+            temp_right--;
+        }
+        while (temp_left < temp_right && arrey[temp_left] <= standard) {
+            temp_left++;
+        }
+        // Swap
+        int temp = arrey[temp_left];
+        arrey[temp_left] = arrey[temp_right];
+        arrey[temp_right] = temp;
+    }
+    int temp = arrey[left];
+    arrey[left] = arrey[temp_left];
+    arrey[temp_left] = temp;
+    quick_sort_standard_optimization(arrey, left, temp_left - 1);
+    quick_sort_standard_optimization(arrey, temp_left + 1, right);
 };
